92262856b77bd2d87e6b089a625d9af3e672bbf8
Kevin Bernfeld <kcbernfeld@gmail.com>
2020-07-22 13:13:49 -0400

    removing unnecessary property

--- chart/Startup.cs
+++ chart/Startup.cs
@@ -22,7 +22,6 @@ namespace planar.server {
     }
 
     public IConfiguration Configuration { get; }
-    private readonly IWebHostEnvironment _env;
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
