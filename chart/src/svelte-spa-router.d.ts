87620401d2104296ab2a57e0cd9e75797b2c80b7
Kevin Bernfeld <kcbernfeld@gmail.com>
2020-08-11 13:54:26 -0400

    shutting up typescript

---
+++ chart/src/svelte-spa-router.d.ts
@@ -0,0 +1,11 @@
+declare module 'svelte-spa-router'; // {
+//   // export function wrap(route: SvelteComponent, userData: Object, ...conditions: Function[]): Object;
+//   export function push(location: string): Promise<any>;
+//   export function pop(): Promise<any>;
+//   export function replace(location: string): Promise<any>;
+//   export function link(node: HTMLElement, hrefVar: string): {
+//     update(updated: any): void;
+//   }
+//   export function nextTickPromise(cb: Function): Promise<any>;
+//   export default SvelteComponent;
+// }
